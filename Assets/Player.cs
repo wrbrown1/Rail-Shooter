99f28809b7ed7bc25ddf8e17aa0244c9898d1344
Will <wrbrown@aggies.ncat.edu>
2019-10-15 15:34:58 -0400

    Added bullets and explosion particle systems

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,11 +10,18 @@ public class Player : MonoBehaviour
     [Tooltip("in m/s")] [SerializeField] float xSpeed = 4f;
     [Tooltip("in m/s")] [SerializeField] float ySpeed = 4f;
 
+    float xThrow, yThrow;
+
     [Tooltip("how far the ship can go left")] [SerializeField] float leftClamp = -3.5f;
     [Tooltip("how far the ship can go right")] [SerializeField] float rightClamp = 3.5f;
 
     [Tooltip("how far the ship can go down")] [SerializeField] float downClamp = -1.5f;
     [Tooltip("how far the ship can go up")] [SerializeField] float upClamp = 1.5f;
+
+    [SerializeField] float positionPitchFactor = 5f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlThrowFactor = -10f;
+    [SerializeField] float controlRollFactor = -10f;
 
     // Start is called before the first frame update
     void Start()
@@ -31,13 +38,17 @@ public class Player : MonoBehaviour
 
     private void ProcessRotation()
     {
-        transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlThrowFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private void ProcessTranslation()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");//This gets a value between -1 and 1 depending on user input
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");//This gets a value between -1 and 1 depending on user input
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = xThrow * xSpeed * Time.deltaTime;//This gives us a value that is framerate independent and is modifiable in Unity
         float yOffset = yThrow * ySpeed * Time.deltaTime;
