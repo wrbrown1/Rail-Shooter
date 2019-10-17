500e8f94f8fbf320da9f591c5ab8621f03b8ff0c
Will <wrbrown@aggies.ncat.edu>
2019-10-16 23:50:43 -0400

    Added enamy explosions

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+    new Collider collider;
+
+    private void Start()
+    {
+        AddBoxCollider();
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+
+    void AddBoxCollider()
+    {
+        collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+}
